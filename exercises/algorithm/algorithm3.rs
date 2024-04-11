dcd1051a2e4071c52dc2f8ffa4bd7a041cc746a8
Hao2203 <516501366@qq.com>
2024-04-11 17:52:27 +0800

    update

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -1,12 +1,52 @@
 /*
-	sort
-	This problem requires you to implement a sorting algorithm
-	you can use bubble sorting, insertion sorting, heap sorting, etc.
+    sort
+    This problem requires you to implement a sorting algorithm
+    you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
-// I AM NOT DONE
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+use std::fs::read_link;
+
+fn sort<T: Ord>(array: &mut [T]) {
+    //TODO
+    recursion(array);
+}
+
+fn recursion<T: Ord>(array: &mut [T]) {
+    let len = array.len();
+    if len < 2 {
+        return;
+    } else if len == 2 {
+        if array[0] > array[1] {
+            array.swap(0, 1);
+        }
+        return;
+    } else {
+        let mid = 0;
+        let mut low = 0;
+        let mut hight = array.len() - 1;
+        while low < hight {
+            low = array
+                .iter()
+                .enumerate()
+                .take_while(|(_, item)| **item > array[0])
+                .next()
+                .map(|item| item.0)
+                .unwrap_or(hight);
+            hight = array[low..len - 1]
+                .iter()
+                .enumerate()
+                .rev()
+                .take_while(|(_, item)| **item < array[0])
+                .next()
+                .map(|item| item.0)
+                .unwrap_or(low);
+            array.swap(low, hight);
+        }
+        array.swap(0, low);
+        let (a1, a2) = array.split_at_mut(low);
+        recursion(a1);
+        recursion(a2);
+    }
 }
 #[cfg(test)]
 mod tests {
@@ -18,13 +58,13 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
